a581e80e1aa30e7c33b2170511da72923049875c
Ariel Vallese <alariva@gmail.com>
2016-02-03 21:31:25 -0300

    WIP: Start rewrite of Concierge code

--- src/Concierge.php
+++ src/Concierge.php
@@ -4,10 +4,9 @@ namespace Timegridio\Concierge;
 
 use Carbon\Carbon;
 use Timegridio\Concierge\Booking\Strategies\BookingStrategy;
-use Timegridio\Concierge\Models\Appointment;
 use Timegridio\Concierge\Models\Business;
-use Timegridio\Concierge\Models\Contact;
 use Timegridio\Concierge\Models\Service;
+use Timegridio\Concierge\VacancyManager;
 
 /*******************************************************************************
  * Concierge Service Layer
@@ -15,23 +14,75 @@ use Timegridio\Concierge\Models\Service;
  ******************************************************************************/
 class Concierge
 {
-    protected $userId;
+    protected $business = null;
 
-    protected $business;
+    protected $strategy = null;
 
-    protected $contact;
+    protected $vacancyManager = null;
 
-    protected $service;
-
-    protected $datetime;
-
-    protected $timezone;
-
-    protected $comments;
-
-    protected $calendar;
-
-    public function takeReservation()
+    public function business($business)
     {
+        $this->business = $business;
+
+        return $this;
+    }
+
+    protected function strategy()
+    {
+        if ($this->strategy === null) {
+            $this->strategy = new BookingStrategy($this->business->strategy);
+        }
+
+        return $this->strategy;
+    }
+
+    protected function vacancyManager()
+    {
+        if($this->vacancyManager === null)
+        {
+            $this->vacancyManager = new VacancyManager();
+        }
+
+        $this->vacancyManager->setBusiness($this->business);
+
+        return $this->vacancyManager;
+    }
+
+    public function takeReservation($request)
+    {
+        $datetime = $this->makeDateTimeUTC($request['date'], $request['time'], $request['timezone']);
+
+        $appointment = $this->strategy()->generateAppointment(
+            $request['issuer'],
+            $this->business,
+            $request['contact'],
+            $request['service'],
+            $datetime,
+            $request['comments']
+        );
+
+        if ($appointment->duplicates()) {
+            return $appointment;
+            // throw new \Exception('Duplicated Appointment Attempted');
+        }
+
+        $vacancy = $this->vacancyManager()->getSlotFor($appointment->start_at, $appointment->service->id);
+
+        if ($vacancy != null && $this->strategy()->hasRoom($appointment, $vacancy)) {
+            $appointment->vacancy()->associate($vacancy);
+            $appointment->save();
+            return $appointment;
+        }
+        return false;
+    }
+
+    protected function makeDateTimeUTC($date, $time, $timezone = null)
+    {
+        if($timezone === null)
+        {
+            $timezone = $this->business->timezone;
+        }
+
+        return Carbon::parse("{$date} {$time} {$timezone}")->timezone('UTC');
     }
 }
---
+++ tests/integration/concierge/ConciergeUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Timegridio\Concierge\Concierge;
+use Timegridio\Concierge\Models\Appointment;
+
+class ConciergeUnitTest extends TestCaseDB
+{
+    use DatabaseTransactions;
+    use ArrangeFixture, CreateUser, CreateContact, CreateBusiness, CreateService, CreateVacancy;
+
+    protected $concierge;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->concierge = new Concierge();
+        $this->arrangeFixture();
+
+    }
+
+    /**
+     * @test
+     */
+    public function it_takes_a_reservation()
+    {
+        $reservationRequest = [
+            'issuer'   => $this->owner,
+            'contact'  => $this->contact,
+            'date'     => '2016-02-04',
+            'time'     => '10:30:00',
+            'timezone' => $this->business->timezone,
+            'service'  => $this->service,
+            'comments' => 'test dont test',
+        ];
+
+        $appointment = $this->concierge
+                            ->business($this->business)
+                            ->takeReservation($reservationRequest);
+
+        $this->assertInstanceOf(Appointment::class, $appointment);
+
+        $this->assertEquals($appointment->issuer->id, $this->owner->id);
+        $this->assertEquals($appointment->contact->name, $this->contact->name);
+        $this->assertEquals($appointment->service->name, $this->service->name);
+        $this->assertEquals($appointment->date, $reservationRequest['date']);
+        $this->assertEquals($appointment->comments, 'test dont test');
+        $this->assertTrue($appointment->exists);
+        $this->assertEquals(32, strlen($appointment->hash));
+    }
+}
