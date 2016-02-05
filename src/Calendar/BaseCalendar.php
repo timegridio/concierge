91235d9a64ea7a281f0c91560ff910efd3fc8538
Ariel Vallese <alariva@gmail.com>
2016-02-05 10:35:34 -0300

    Restore default behavior of BaseCalendar

--- src/Calendar/BaseCalendar.php
+++ src/Calendar/BaseCalendar.php
@@ -76,35 +76,35 @@ abstract class BaseCalendar
         return Carbon::parse($this->date);
     }
 
-//    protected function find()
-//    {
-//        $this->prepare();
-//
-//        $results = $this->vacancies->get();
-//
-//        $fromDatetime = $this->getUTCDateTime();
-//
-//        if ($this->duration !== null) {
-//            $toDatetime = $fromDatetime->addMinutes($this->duration);
-//
-//            $results = $results->reject(function ($vacancy) use ($fromDatetime, $toDatetime) {
-//                return !$vacancy->hasRoomBetween($fromDatetime, $toDatetime);
-//            });
-//        }
-//
-//        return $results;
-//    }
-//
-//    protected function prepare()
-//    {
-//        if ($this->service !== null) {
-//            $this->vacancies->forService($this->service);
-//        }
-//
-//        if ($this->date !== null &&  $this->time !== null) {
-//            $this->vacancies->forDateTime($this->getUTCDateTime());
-//        }
-//
-//        return $this;
-//    }
+    public function find()
+    {
+        $this->prepare();
+
+        $results = $this->vacancies->get();
+
+        $fromDatetime = $this->getUTCDateTime();
+
+        if ($this->duration !== null) {
+            $toDatetime = $fromDatetime->addMinutes($this->duration);
+
+            $results = $results->reject(function ($vacancy) use ($fromDatetime, $toDatetime) {
+                return !$vacancy->hasRoomBetween($fromDatetime, $toDatetime);
+            });
+        }
+
+        return $results;
+    }
+
+    protected function prepare()
+    {
+        if ($this->service !== null) {
+            $this->vacancies->forService($this->service);
+        }
+
+        if ($this->date !== null &&  $this->time !== null) {
+            $this->vacancies->forDateTime($this->getUTCDateTime());
+        }
+
+        return $this;
+    }
 }
