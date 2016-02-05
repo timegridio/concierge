f4becae15bedd91b02b349538a9c283575f7ef0f
Ariel Vallese <alariva@gmail.com>
2016-02-05 11:23:49 -0300

    Switch methods to abstract

--- src/Calendar/BaseCalendar.php
+++ src/Calendar/BaseCalendar.php
@@ -76,35 +76,7 @@ abstract class BaseCalendar
         return Carbon::parse($this->date);
     }
 
-    public function find()
-    {
-        $this->prepare();
+    abstract public function find();
 
-        $results = $this->vacancies->get();
-
-        $fromDatetime = $this->getUTCDateTime();
-
-        if ($this->duration !== null) {
-            $toDatetime = $fromDatetime->addMinutes($this->duration);
-
-            $results = $results->reject(function ($vacancy) use ($fromDatetime, $toDatetime) {
-                return !$vacancy->hasRoomBetween($fromDatetime, $toDatetime);
-            });
-        }
-
-        return $results;
-    }
-
-    protected function prepare()
-    {
-        if ($this->service !== null) {
-            $this->vacancies->forService($this->service);
-        }
-
-        if ($this->date !== null &&  $this->time !== null) {
-            $this->vacancies->forDateTime($this->getUTCDateTime());
-        }
-
-        return $this;
-    }
+    abstract protected function prepare();
 }
