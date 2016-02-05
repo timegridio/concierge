c9db9bb8f7aa69ba7e453347deaebeb2171a37f2
Ariel Vallese <alariva@gmail.com>
2016-02-05 10:31:25 -0300

    Add Exceptions

---
+++ src/Exceptions/StrategyMethodNotRecognizedException.php
@@ -0,0 +1,12 @@
+<?php namespace Timegridio\Concierge\Exceptions;
+
+use Exception;
+
+/**
+ * Class StrategyMethodNotRecognizedException
+ *
+ * @package Timegridio\Concierge\Exceptions
+ */
+class StrategyMethodNotRecognizedException extends Exception
+{
+}
---
+++ src/Exceptions/StrategyNotRecognizedException.php
@@ -0,0 +1,12 @@
+<?php namespace Timegridio\Concierge\Exceptions;
+
+use Exception;
+
+/**
+ * Class StrategyNotRecognizedException
+ *
+ * @package Timegridio\Concierge\Exceptions
+ */
+class StrategyNotRecognizedException extends Exception
+{
+}
