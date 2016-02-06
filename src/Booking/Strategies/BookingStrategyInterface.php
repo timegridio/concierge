7d515f50b9a6b47aecae7ea5fcc4ecf717865f50
Ariel Vallese <alariva@gmail.com>
2016-02-06 07:13:39 -0300

    Define interface

--- src/Booking/Strategies/BookingStrategyInterface.php
+++ src/Booking/Strategies/BookingStrategyInterface.php
@@ -2,6 +2,11 @@
 
 namespace Timegridio\Concierge\Booking\Strategies;
 
+use Timegridio\Concierge\Booking\Timetable;
+
 interface BookingStrategyInterface
 {
+    public function __construct(Timetable $timetable);
+
+    public function buildTimetable($vacancies, $starting = 'today', $days = 1);
 }
