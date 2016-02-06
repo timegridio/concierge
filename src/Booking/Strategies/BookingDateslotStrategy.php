79fd48f46daaa519f9bf57b6b99d09902ca079b3
Ariel Vallese <alariva@gmail.com>
2016-02-06 07:13:29 -0300

    Add dateslot booking strategy implementation

--- src/Booking/Strategies/BookingDateslotStrategy.php
+++ src/Booking/Strategies/BookingDateslotStrategy.php
@@ -2,6 +2,54 @@
 
 namespace Timegridio\Concierge\Booking\Strategies;
 
+use Timegridio\Concierge\Booking\Timetable;
+use Timegridio\Concierge\Models\Vacancy;
+
 class BookingDateslotStrategy implements BookingStrategyInterface
 {
+    private $timetable;
+
+    private $interval = 30;
+
+    public function __construct(Timetable $timetable)
+    {
+        $this->timetable = $timetable;
+    }
+
+    /**
+     * Build timetable.
+     *
+     * @param Illuminate\Database\Eloquent\Collection $vacancies
+     * @param string                                  $starting
+     * @param int                                     $days
+     *
+     * @return array
+     */
+    public function buildTimetable($vacancies, $starting = 'today', $days = 1)
+    {
+        $this->timetable
+             ->interval($this->interval)
+             ->format('date.service.time')
+             ->from($starting)
+             ->future($days)
+             ->init();
+
+        foreach ($vacancies as $vacancy) {
+            $this->updateTimeslots($vacancy, $this->interval);
+        }
+
+        return $this->timetable->get();
+    }
+
+    protected function updateTimeslots(Vacancy $vacancy, $step = 30)
+    {
+        $fromTime = $vacancy->start_at;
+        $toTime = $vacancy->finish_at;
+
+        $capacity = $vacancy->getAvailableCapacityBetween($fromTime->timezone('UTC'), $toTime->timezone('UTC'));
+
+        $time = $fromTime->timezone($vacancy->business->timezone)->format('H:i:s');
+
+        $this->timetable->capacity($vacancy->date, $time, $vacancy->service->slug, $capacity);
+    }
 }
---
+++ tests/integration/concierge/BookingDateslotStrategyUnitTest.php
@@ -0,0 +1,136 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Timegridio\Concierge\Booking\Strategies\BookingStrategy;
+
+class BookingDateslotStrategyUnitTest extends TestCaseDB
+{
+    use DatabaseTransactions;
+    use ArrangeFixture, CreateUser, CreateContact, CreateBusiness, CreateService, CreateVacancy, CreateAppointment;
+
+    /**
+     * @test
+     * @expectedException Timegridio\Concierge\Exceptions\StrategyNotRecognizedException
+     */
+    public function it_rejects_unrecognized_strategies()
+    {
+        new BookingStrategy('invalid');
+    }
+
+    /**
+     * @test
+     */
+    public function it_generates_a_dateslot_timetable()
+    {
+        $this->arrangeFixture();
+
+        $bookingStrategy = new BookingStrategy('dateslot');
+
+        $vacancies = $this->business->vacancies()->with('appointments')->with('service')->get();
+
+        $timetable = $bookingStrategy->buildTimetable($vacancies);
+
+        $timezone = $this->vacancy->business->timezone;
+
+        $expected = [
+            $this->vacancy->date => [
+                $this->vacancy->service->slug => [
+                    $this->vacancy->start_at->timezone($timezone)->toTimeString() => $this->vacancy->capacity,
+                    ],
+                ],
+            ];
+
+        $this->assertArraySubset($expected, $timetable);
+    }
+
+    /**
+     * @test
+     */
+    public function it_generates_a_dateslot_timetable_and_discounts_one_booked_appointment()
+    {
+        $this->arrangeFixture();
+
+        $bookingStrategy = new BookingStrategy('timeslot');
+
+        $timezone = $this->vacancy->business->timezone;
+
+        $appointment = $this->createAppointment([
+            'business_id' => $this->business->id,
+            'contact_id'  => $this->contact->id,
+            'service_id'  => $this->service->id,
+            'vacancy_id'  => $this->vacancy->id,
+            'status'      => 'C',
+            'start_at'    => Carbon::parse("{$this->vacancy->date} 10:00:00 ".$timezone)->timezone('UTC'),
+            'finish_at'   => Carbon::parse("{$this->vacancy->date} 10:30:00 ".$timezone)->timezone('UTC'),
+            'duration'    => 30,
+            'comments'    => 'test dont test',
+            ]);
+
+        $vacancies = $this->business->vacancies()->with('appointments')->with('service')->get();
+
+        $timetable = $bookingStrategy->buildTimetable($vacancies);
+
+        $expected = [
+            $this->vacancy->date => [
+                $appointment->service->slug => [
+                    $appointment->start_at->timezone($timezone)->toTimeString() => $this->vacancy->capacity - 1,
+                    ],
+                ],
+            ];
+
+        $this->assertArraySubset($expected, $timetable);
+    }
+
+    /**
+     * @test
+     */
+    public function it_generates_a_dateslot_timetable_and_discounts_two_booked_appointments()
+    {
+        $this->arrangeFixture();
+
+        $bookingStrategy = new BookingStrategy('timeslot');
+
+        $timezone = $this->vacancy->business->timezone;
+
+        $appointmentOne = $this->createAppointment([
+            'business_id' => $this->business->id,
+            'contact_id'  => $this->contact->id,
+            'service_id'  => $this->service->id,
+            'vacancy_id'  => $this->vacancy->id,
+            'status'      => 'C',
+            'start_at'    => Carbon::parse("{$this->vacancy->date} 10:00:00 ".$timezone)->timezone('UTC'),
+            'finish_at'   => Carbon::parse("{$this->vacancy->date} 10:30:00 ".$timezone)->timezone('UTC'),
+            'duration'    => 30,
+            'comments'    => 'test dont test',
+            ]);
+
+        $contactTwo = $this->createContact();
+
+        $appointmentTwo = $this->createAppointment([
+            'business_id' => $this->business->id,
+            'contact_id'  => $contactTwo->id,
+            'service_id'  => $this->service->id,
+            'vacancy_id'  => $this->vacancy->id,
+            'status'      => 'C',
+            'start_at'    => Carbon::parse("{$this->vacancy->date} 10:00:00 ".$this->business->timezone)->timezone('UTC'),
+            'finish_at'   => Carbon::parse("{$this->vacancy->date} 10:30:00 ".$this->business->timezone)->timezone('UTC'),
+            'duration'    => 30,
+            'comments'    => 'test dont test',
+            ]);
+
+        $vacancies = $this->business->vacancies()->with('appointments')->with('service')->get();
+
+        $timetable = $bookingStrategy->buildTimetable($vacancies);
+
+        $expected = [
+            $this->vacancy->date => [
+                $appointmentOne->service->slug => [
+                    $appointmentOne->start_at->timezone($timezone)->toTimeString() => $this->vacancy->capacity - 2,
+                    ],
+                ],
+            ];
+
+        $this->assertArraySubset($expected, $timetable);
+    }
+}
