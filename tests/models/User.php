ecfb80a500109413abd2c903a3f433efb2134042
Ariel Vallese <alariva@gmail.com>
2016-02-09 19:57:17 -0300

    Simplify stub class

--- tests/models/User.php
+++ tests/models/User.php
@@ -3,11 +3,8 @@
 namespace Timegridio\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Timegridio\Concierge\Traits\OwnsBusinesses;
 
 class User extends Model
 {
-    use OwnsBusinesses;
-
     protected $fillable = ['id'];
 }
