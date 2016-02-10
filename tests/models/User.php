d3d2a84ef273a39f59d65d84cf057ae97156d7d2
Ariel Vallese <alariva@gmail.com>
2016-02-09 22:38:10 -0300

    Restore User stub as ecfb80 broke tests

--- tests/models/User.php
+++ tests/models/User.php
@@ -3,8 +3,11 @@
 namespace Timegridio\Tests\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Timegridio\Concierge\Traits\OwnsBusinesses;
 
 class User extends Model
 {
+    use OwnsBusinesses;
+
     protected $fillable = ['id'];
 }
