71401254ae4a729a60614786316d5036702198b8
Ariel Vallese <alariva@gmail.com>
2016-02-11 11:25:42 -0300

    Turn BaseTimetableStrategy to abstract

--- src/Timetable/Strategies/BaseTimetableStrategy.php
+++ src/Timetable/Strategies/BaseTimetableStrategy.php
@@ -4,16 +4,9 @@ namespace Timegridio\Concierge\Timetable\Strategies;
 
 use Timegridio\Concierge\Timetable\Timetable;
 
-class BaseTimetableStrategy
+abstract class BaseTimetableStrategy
 {
     protected $timetable;
 
-    protected function initTimetable($starting, $days)
-    {
-        $this->timetable
-             ->format('date.service.time')
-             ->from($starting)
-             ->future($days)
-             ->init();
-    }
+    abstract protected function initTimetable($starting, $days);
 }
