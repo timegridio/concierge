c3e779c40d492c00dd7dc64855036e31afef8097
Ariel Vallese <alariva@gmail.com>
2016-02-11 16:37:38 -0300

    Increase test coverage

---
+++ tests/unit/models/ServiceTypeTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Timegridio\Concierge\Models\ServiceType;
+
+class ServiceTypeTest extends TestCaseDB
+{
+    use DatabaseTransactions;
+    use CreateService;
+
+    /**
+     * @test
+     */
+    public function it_has_services()
+    {
+        $serviceType = new ServiceType;
+
+        $this->assertInstanceOf(HasMany::class, $serviceType->services());
+    }
+
+    /**
+     * @test
+     */
+    public function it_belongs_to_businesses()
+    {
+        $serviceType = new ServiceType;
+
+        $this->assertInstanceOf(BelongsTo::class, $serviceType->businesses());
+    }
+}
