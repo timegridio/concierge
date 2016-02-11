0574b5e8c224d3f21f77848aa5d894656ac2f572
Ariel Vallese <alariva@gmail.com>
2016-02-11 17:56:37 -0300

    Enhance test coverage

---
+++ tests/unit/presenters/BusinessPresenterTest.php
@@ -0,0 +1,112 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Timegridio\Concierge\Presenters\BusinessPresenter;
+
+class BusinessPresenterTest extends TestCaseDB
+{
+    use DatabaseTransactions;
+    use CreateBusiness;
+
+    protected $businessPresenter;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->businessPresenter = $this->createBusinessPresenter();
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_presenter()
+    {
+        $business = $this->createBusinessPresenter();
+
+        $this->assertEquals(BusinessPresenter::class, $business->getPresenterClass());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_facebook_image()
+    {
+        $business = $this->createBusinessPresenter([
+            'social_facebook' => 'https://www.facebook.com/timegrid.io/',
+            ]);
+
+        $img = $business->facebookImg();
+
+        $expected = "<img class=\"img-thumbnail media-object\" src='http://graph.facebook.com/timegrid.io/picture?type=square' />";
+
+        $this->assertInternalType('string', $img);
+        $this->assertEquals($expected, $img);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_facebook_empty_image_placeholder()
+    {
+        $business = $this->createBusinessPresenter([
+            'social_facebook' => null,
+            ]);
+
+        $img = $business->facebookImg();
+
+        $expected = '<img class="img-thumbnail" src="//placehold.it/100x100"/>';
+
+        $this->assertInternalType('string', $img);
+        $this->assertEquals($expected, $img);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_static_map()
+    {
+        $business = $this->createBusinessPresenter();
+
+        $map = $business->staticMap();
+
+        $this->assertInternalType('string', $map);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_industry_icon()
+    {
+        $business = $this->createBusinessPresenter([
+            'category_id' => 'factory:Timegridio\Concierge\Models\Category',
+            ]);
+
+        $icon = $business->industryIcon();
+
+        $this->assertInternalType('string', $icon);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_profile_fulfilment_quality_score()
+    {
+        $business = $this->createBusinessPresenter();
+
+        $quality = $business->quality();
+
+        $this->assertInternalType('float', $quality);
+        $this->assertGreaterThan(0, $quality);
+        $this->assertLessThan(100, $quality);
+    }
+
+    /////////////
+    // Helpers //
+    /////////////
+
+    protected function createBusinessPresenter($overrides = [])
+    {
+        return new BusinessPresenter($this->createBusiness($overrides));
+    }
+}
