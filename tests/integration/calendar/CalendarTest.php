fe83d6c4a16825c080ab44c267965f29313b7f67
Ariel Vallese <alariva@gmail.com>
2016-02-11 18:27:53 -0300

    Increase test coverage

--- tests/integration/calendar/CalendarTest.php
+++ tests/integration/calendar/CalendarTest.php
@@ -16,4 +16,17 @@ class CalendarTest extends TestCaseDB
 
         new Calendar('unknown-strategy', $business->vacancies());
     }
+
+    /**
+     * @test
+     * @expectedException Timegridio\Concierge\Exceptions\StrategyMethodNotRecognizedException
+     */
+    public function it_rejects_an_unknown_method()
+    {
+        $business = $this->createBusiness();
+
+        $calendar = new Calendar('dateslot', $business->vacancies());
+
+        $calendar->anUnknownMethod();
+    }
 }
