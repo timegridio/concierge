038dc7717aed03becb2ba5a5623da549b7403472
Ariel Vallese <alariva@gmail.com>
2016-02-11 16:59:48 -0300

    Disable unused methods
    Most of them should be re-implemented at another layer or 
    adjusted.

--- src/Presenters/AppointmentPresenter.php
+++ src/Presenters/AppointmentPresenter.php
@@ -35,42 +35,42 @@ class AppointmentPresenter extends BasePresenter
                     ->format($format);
     }
 
-    public function arriveAt()
-    {
-        if (!$this->wrappedObject->business->pref('appointment_flexible_arrival')) {
-            return $this->time;
-        }
+//    public function arriveAt()
+//    {
+//        if (!$this->wrappedObject->business->pref('appointment_flexible_arrival')) {
+//            return $this->time;
+//        }
+//
+//        $fromTime = $this->wrappedObject
+//                         ->vacancy
+//                         ->start_at
+//                         ->timezone($this->wrappedObject->business->timezone)
+//                         ->format(config('root.time.format'));
+//
+//        $toTime = $this->wrappedObject
+//                       ->vacancy
+//                       ->finish_at
+//                       ->timezone($this->wrappedObject->business->timezone)
+//                       ->format(config('root.time.format'));
+//
+//        return ucwords(trans('Concierge::appointments.text.from_to', ['from' => $fromTime, 'to' => $toTime]));
+//    }
 
-        $fromTime = $this->wrappedObject
-                         ->vacancy
-                         ->start_at
-                         ->timezone($this->wrappedObject->business->timezone)
-                         ->format(config('root.time.format'));
+//    public function time()
+//    {
+//        return $this->wrappedObject
+//                    ->start_at
+//                    ->timezone($this->wrappedObject->business->timezone)
+//                    ->format(config('root.time.format'));
+//    }
 
-        $toTime = $this->wrappedObject
-                       ->vacancy
-                       ->finish_at
-                       ->timezone($this->wrappedObject->business->timezone)
-                       ->format(config('root.time.format'));
-
-        return ucwords(trans('Concierge::appointments.text.from_to', ['from' => $fromTime, 'to' => $toTime]));
-    }
-
-    public function time()
-    {
-        return $this->wrappedObject
-                    ->start_at
-                    ->timezone($this->wrappedObject->business->timezone)
-                    ->format(config('root.time.format'));
-    }
-
-    public function finishTime()
-    {
-        return $this->wrappedObject
-                    ->finish_at
-                    ->timezone($this->wrappedObject->business->timezone)
-                    ->format(config('root.time.format'));
-    }
+//    public function finishTime()
+//    {
+//        return $this->wrappedObject
+//                    ->finish_at
+//                    ->timezone($this->wrappedObject->business->timezone)
+//                    ->format(config('root.time.format'));
+//    }
 
     public function diffForHumans()
     {
@@ -87,20 +87,20 @@ class AppointmentPresenter extends BasePresenter
         return $this->wrappedObject->business->postal_address;
     }
 
-    public function statusLetter()
-    {
-        return substr(trans('Concierge::appointments.status.'.$this->wrappedObject->statusLabel), 0, 1);
-    }
+//    public function statusLetter()
+//    {
+//        return substr(trans('Concierge::appointments.status.'.$this->wrappedObject->statusLabel), 0, 1);
+//    }
 
-    public function status()
-    {
-        return trans('Concierge::appointments.status.'.$this->wrappedObject->statusLabel);
-    }
+//    public function status()
+//    {
+//        return trans('Concierge::appointments.status.'.$this->wrappedObject->statusLabel);
+//    }
 
-    public function statusIcon()
-    {
-        return '<span class="label label-'.$this->statusToCssClass().'">'.$this->statusLetter.'</span>';
-    }
+//    public function statusIcon()
+//    {
+//        return '<span class="label label-'.$this->statusToCssClass().'">'.$this->statusLetter.'</span>';
+//    }
 
     public function statusToCssClass()
     {
@@ -120,13 +120,13 @@ class AppointmentPresenter extends BasePresenter
         }
     }
 
-    public function panel()
-    {
-        return view('widgets.appointment.panel._body', ['appointment' => $this, 'user' => auth()->user()])->render();
-    }
+//    public function panel()
+//    {
+//        return view('widgets.appointment.panel._body', ['appointment' => $this, 'user' => auth()->user()])->render();
+//    }
 
-    public function row()
-    {
-        return view('widgets.appointment.row._body', ['appointment' => $this, 'user' => auth()->user()])->render();
-    }
+//    public function row()
+//    {
+//        return view('widgets.appointment.row._body', ['appointment' => $this, 'user' => auth()->user()])->render();
+//    }
 }
