c1caf96f00c4891977459dadd049002aa014109e
Ariel Vallese <alariva@gmail.com>
2016-03-01 17:27:33 -0300

    Increase test coverage

--- tests/integration/vacancy/VacancyManagerUnitTest.php
+++ tests/integration/vacancy/VacancyManagerUnitTest.php
@@ -4,15 +4,18 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Timegridio\Concierge\Models\Vacancy;
 use Timegridio\Concierge\Vacancy\VacancyManager;
+use Timegridio\Concierge\Vacancy\VacancyParser;
 
 class VacancyManagerUnitTest extends TestCaseDB
 {
     use DatabaseTransactions;
-    use CreateBusiness, CreateService;
+    use ArrangeFixture, CreateUser, CreateBusiness, CreateService, CreateAppointment, CreateContact, CreateVacancy;
 
     protected $business;
 
-    protected $vacancy;
+    protected $vacancyManager;
+
+    protected $vacancyParser;
 
     public function setUp()
     {
@@ -20,7 +23,9 @@ class VacancyManagerUnitTest extends TestCaseDB
 
         $this->business = $this->createBusiness();
 
-        $this->vacancy = new VacancyManager($this->business);
+        $this->vacancyManager = new VacancyManager($this->business);
+
+        $this->vacancyParser = new VacancyParser();
     }
 
     /**
@@ -36,7 +41,7 @@ class VacancyManagerUnitTest extends TestCaseDB
         $service = $this->createService();
         $capacity = 5;
 
-        $vacancy = $this->vacancy->publish($date, $startAt, $finishAt, $service->id, $capacity);
+        $vacancy = $this->vacancyManager->publish($date, $startAt, $finishAt, $service->id, $capacity);
 
         $this->assertInstanceOf(Vacancy::class, $vacancy);
         $this->assertEquals($vacancy->date, $date);
@@ -44,5 +49,33 @@ class VacancyManagerUnitTest extends TestCaseDB
         $this->assertEquals($vacancy->finish_at, $finishAt);
         $this->assertEquals($vacancy->capacity, $capacity);
         $this->assertEquals($vacancy->service->id, $service->id);
+    }
+
+    /**
+     * @test
+     */
+    public function it_publishes_a_batch_vacancy_statement()
+    {
+        $this->arrangeFixture();
+
+        $capacity = 1;
+
+        $vacancyStatement = <<<EOD
+{$this->service->slug}:{$capacity}
+ mon,tue
+  8-12,14-20
+EOD;
+
+        $publishedVacancies = $this->vacancyParser->parseStatements($vacancyStatement);
+
+        $this->vacancyManager->updateBatch($this->business, $publishedVacancies);
+
+        $vacancies = $this->business->vacancies()->get();
+
+        foreach ($vacancies as $vacancy) {
+            $this->assertInstanceOf(Vacancy::class, $vacancy);
+            $this->assertEquals($vacancy->capacity, $capacity);
+            $this->assertEquals($vacancy->service->id, $this->service->id);
+        }
     }
 }
