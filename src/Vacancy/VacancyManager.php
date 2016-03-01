867505464e0d13b8aef5f8ec304f2ce46811d97e
Ariel Vallese <alariva@gmail.com>
2016-02-29 21:01:27 -0300

    Add VacancyManager advanced mode

--- src/Vacancy/VacancyManager.php
+++ src/Vacancy/VacancyManager.php
@@ -4,6 +4,7 @@ namespace Timegridio\Concierge\Vacancy;
 
 use Carbon\Carbon;
 use Timegridio\Concierge\Models\Business;
+use Timegridio\Concierge\Models\Service;
 use Timegridio\Concierge\Models\Vacancy;
 
 class VacancyManager
@@ -16,21 +17,98 @@ class VacancyManager
     }
 
     /**
-     * Get a Vacancy for a given DateTime and Service combination.
+     * Update vacancies from batch statements.
      *
-     * @param Carbon  $targetDateTime
-     * @param int $serviceId
+     * @param Business $business
+     * @param array    $parsedStatements
      *
-     * @return Timegridio\Concierge\Models\Vacancy
+     * @return bool
      */
-//    public function getSlotFor(Carbon $targetDateTime, $serviceId)
-//    {
-//        return $this->business
-//            ->vacancies()
-//            ->forDateTime($targetDateTime)
-//            ->forService($serviceId)
-//            ->first();
-//    }
+    public function updateBatch(Business $business, $parsedStatements)
+    {
+        $changed = false;
+        $dates = $this->arrayGroupBy('date', $parsedStatements);
+
+        foreach ($dates as $date => $statements) {
+            $services = $this->arrayGroupBy('service', $statements);
+
+            $changed |= $this->processServiceStatements($business, $date, $services);
+        }
+
+        return $changed;
+    }
+
+    protected function processServiceStatements($business, $date, $services)
+    {
+        $changed = false;
+        foreach ($services as $serviceSlug => $statements) {
+            $service = Service::where('slug', $serviceSlug)->get()->first();
+
+            if ($service === null) {
+
+                //  Invalid services are skipped to avoid user frustration.
+                //  TODO: Still, a user-level WARNING should be raised with no fatal error
+
+                continue;
+            }
+
+            $vacancy = $business->vacancies()->forDate(Carbon::parse($date))->forService($service->id);
+
+            if ($vacancy) {
+                $vacancy->delete();
+            }
+
+            $changed |= $this->processStatements($business, $date, $service, $statements);
+        }
+
+        return $changed;
+    }
+
+    protected function processStatements($business, $date, $service, $statements)
+    {
+        $changed = false;
+        foreach ($statements as $statement) {
+            $changed |= $this->publishVacancy($business, $date, $service, $statement);
+        }
+
+        return $changed;
+    }
+
+    protected function publishVacancy($business, $date, $service, $statement)
+    {
+        $date = $statement['date'];
+        $startAt = $statement['startAt'];
+        $finishAt = $statement['finishAt'];
+
+        $startAt = Carbon::parse("{$date} {$startAt} {$business->timezone}")->timezone('UTC');
+        $finishAt = Carbon::parse("{$date} {$finishAt} {$business->timezone}")->timezone('UTC');
+
+        $vacancyValues = [
+            'business_id' => $business->id,
+            'service_id'  => $service->id,
+            'date'        => $statement['date'],
+            'capacity'    => intval($statement['capacity']),
+            'start_at'    => $startAt,
+            'finish_at'   => $finishAt,
+            ];
+
+        $vacancy = Vacancy::create($vacancyValues);
+
+        return $vacancy !== null;
+    }
+
+    protected function arrayGroupBy($key, $array)
+    {
+        $grouped = [];
+        foreach ($array as $hash => $item) {
+            if (!array_key_exists($item[$key], $grouped)) {
+                $grouped[$item[$key]] = [];
+            }
+            $grouped[$item[$key]][] = $item;
+        }
+
+        return $grouped;
+    }
 
     public function publish($date, Carbon $startAt, Carbon $finishAt, $serviceId, $capacity = 1)
     {
@@ -47,7 +125,6 @@ class VacancyManager
 
         return Vacancy::updateOrCreate($vacancyKeys, $vacancyValues);
     }
-
 
     /**
      * [generateAvailability description].
@@ -73,5 +150,4 @@ class VacancyManager
 
         return $dates;
     }
-
 }
