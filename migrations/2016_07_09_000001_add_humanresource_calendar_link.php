7e24bc7ae0ea8ea42448ccb2e6e52664bfa0be65
Ariel Vallese <alariva@gmail.com>
2016-07-09 19:58:05 -0300

    Fix build by adding doctrine/dbal require

--- migrations/2016_07_09_000001_add_humanresource_calendar_link.php
+++ migrations/2016_07_09_000001_add_humanresource_calendar_link.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class AddHumanresourceCalendarLink extends Migration
 {
@@ -11,7 +12,7 @@ class AddHumanresourceCalendarLink extends Migration
      */
     public function up()
     {
-        Schema::table('humanresources', function ($table) {
+        Schema::table('humanresources', function (Blueprint $table) {
             $table->string('calendar_link')->nullable();
         });
     }
@@ -23,7 +24,7 @@ class AddHumanresourceCalendarLink extends Migration
      */
     public function down()
     {
-        Schema::table('humanresources', function ($table) {
+        Schema::table('humanresources', function (Blueprint $table) {
             $table->dropColumn('calendar_link');
         });
     }
