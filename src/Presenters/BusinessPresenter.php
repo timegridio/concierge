bf676545694b1287d98ed5de481744f22aec46d7
Ariel Vallese <alariva@gmail.com>
2016-11-07 20:44:39 -0300

    Remove cover_photo_url support

--- src/Presenters/BusinessPresenter.php
+++ src/Presenters/BusinessPresenter.php
@@ -69,8 +69,7 @@ class BusinessPresenter extends BasePresenter
      */
     public function industryIcon()
     {
-        $src = ($this->wrappedObject->pref('cover_photo_url')) ?:
-            asset('/img/industries/'.$this->wrappedObject->category->slug.'.png');
+        $src = asset('/img/industries/'.$this->wrappedObject->category->slug.'.png');
 
         return "<img class=\"img-responsive center-block\" src=\"{$src}\"/>";
     }
